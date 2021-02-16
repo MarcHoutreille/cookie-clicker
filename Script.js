51e9f66d750005fb03e4031b628494f5d0193fc8
Pauline Rohmer <77145751+rohmerpauline@users.noreply.github.com>
2021-02-16 16:26:43 +0100

    Multiplier function

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 let score = 0;
-let multiplier = 0;
+let multiplierLevel = 1;
+let multiplierPower = 1;
 let bonus = 0;
 let autoClicker = 0;
 
@@ -9,10 +10,12 @@ let bonusPrice = 75;
 
 score = document.getElementById('score').innerHTML;
 
-function addToscore(n) {
-    score = parseInt(score) + n;
+/* Add to score when clicked */
+function addToscore(n, multiplierPower, bonus) {
+    score = parseInt(score) + (n * multiplierPower);
     document.getElementById('score').innerHTML = score;
 }
+
 /*update score per second function ?*/
 function buyAutoClicker() {
     if (score >= autoClickerPrice) {
@@ -33,15 +36,67 @@ function buyAutoClicker() {
     }
 }
 
+/*Multiplier*/
 
-setInterval(function(){
-    /*
+if (multiplierLevel < 2) {
+    document.getElementById("active-multiplier").style.display = "none";
+} 
+
+function buyMultiplier () {
+    if (multiplierLevel < 4) {
+        if (score >= multiplierPrice) {
+            score = score - multiplierPrice;
+            multiplierLevel++;
+            multiplierPrice = multiplierPrice * 2;
+            document.getElementById('score').innerHTML = score;
+            document.getElementById("active-multiplier").style.display = "inline";
+            document.getElementById('multiplierPrice').innerHTML = multiplierPrice;
+            switch (multiplierLevel) {
+                case 2 :
+                    document.getElementById("active-multiplier").innerHTML = "x 2";
+                    multiplierPower = 2;
+                break;
+                case 3 :
+                    document.getElementById("active-multiplier").innerHTML = "x 5";
+                    multiplierPower = 5;
+                break;
+                case 4 : 
+                    document.getElementById("active-multiplier").innerHTML = "x 10";
+                    multiplierPower = 10;
+                break;
+            }
+        }
+    } else {
+        // bouton inactif 
+    }
+}
+            
+
+// addToscore(multiplier); quand bonus acheté
+
+/*
+function buyBonus () {
+    if (score >= bonusPrice) {
+        score = score - bonusPrice;
+        bonus = 2;
+        setTimeout(stopBonus(){
+            bonus = 0;
+        }, 30000);
+        document.getElementById('score').innerHTML = score;
+        document.getElementById('bonus').innerHTML = bonus;
+        document.getElementById('bonusPrice').innerHTML = bonusPrice;
+    } else {
+        // bonus price inactif
+    }
+}*/
+
+
+/* setInterval(function(){
     score = score + autoClicker;
-    score = score * multiplier;
+    score = score * multplier;
     score = score * bonus;
-    */
     document.getElementById('score').innerHTML = score;
-}, 1000);
+}, 1000); */
 
 
 
