4c76c87368bb6a6c3758c9645a8941e3b73cb2ab
Thomas Quertinmont <Thomas.quertinmont@gmail.com>
2021-02-16 09:55:53 +0100

    adding js script

---
+++ Script.js
@@ -0,0 +1,20 @@
+let score = 0;
+let multiplier = 0;
+let bonus = 0;
+let autoClicker = 0;
+
+let autoClickerPrice = 25;
+let multiplierPrice = 50;
+let bonusPrice = 75;
+
+document.getElementById('score').innerHTML = score;
+
+setInterval(function(){
+    score = score + autoClicker;
+    score = score * multiplier;
+    score = score * bonus;
+    document.getElementById('score').innerHTML = score;
+}, 1000);
+
+
+
